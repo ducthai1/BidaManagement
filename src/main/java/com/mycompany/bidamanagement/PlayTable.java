f2521f80e1e2f80cc1deddfcacbb693e768958e2
Thái Tăng Đức <thaitangduc1@gmail.com>
2024-01-21 23:28:42 +0700

    up

--- src/main/java/com/mycompany/bidamanagement/PlayTable.java
+++ src/main/java/com/mycompany/bidamanagement/PlayTable.java
@@ -31,7 +31,7 @@ public class PlayTable extends javax.swing.JFrame {
     }
 
     private void updateTimer() {
-        if (milliseconds > 58) {
+        if (milliseconds > 59) {
             milliseconds = 0;
             seconds++;
         }
